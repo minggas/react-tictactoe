bfa9ccba52e7a62580ce01185e730825a3ec985b
minggas <fasfora@gmail.com>
2018-10-09 11:23:49 -0300

    fix result message style

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,32 +5,54 @@ class Result extends React.Component {
     if (!this.props.show) {
       return null;
     }
-    // The gray background
-    const backdropStyle = {
-      position: "fixed",
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: "rgba(0,0,0,0.3)",
-      padding: 50
-    };
-
-    // The modal "window"
-    const modalStyle = {
-      backgroundColor: "#eee",
-      borderRadius: 5,
-      maxWidth: 800,
-      minHeight: 300,
-      margin: "0 auto",
-      padding: 30
+    const styles = {
+      // The gray background
+      backdropStyle: {
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: "rgba(0,0,0,0.3)",
+        padding: "5%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      // The modal "window"
+      modalStyle: {
+        backgroundColor: "#eee",
+        borderRadius: 5,
+        maxWidth: 800,
+        minHeight: 50,
+        margin: "auto",
+        padding: 30
+      },
+      button: {
+        display: "block",
+        margin: "1.5rem auto 0",
+        padding: "0.5rem 1rem",
+        borderRadius: 5,
+        border: "none",
+        backgroundColor: "hsl(200,50%,50%)",
+        color: "hsl(0,0%,98%)",
+        fontSize: "1.3rem",
+        fontWeight: "bold",
+        letterSpacing: 2
+      },
+      title: {
+        textAlign: "center",
+        fontSize: "2.5rem"
+      }
     };
 
     return (
-      <div className="backdrop" style={backdropStyle}>
-        <div className="modal" style={modalStyle}>
-          <h1>{this.props.result}</h1>
-          <button onClick={this.props.closeBtn}>OK</button>
+      <div className="backdrop" style={styles.backdropStyle}>
+        <div className="modal" style={styles.modalStyle}>
+          <h1 style={styles.title}>{this.props.result}</h1>
+          <button onClick={this.props.closeBtn} style={styles.button}>
+            OK
+          </button>
         </div>
       </div>
     );
